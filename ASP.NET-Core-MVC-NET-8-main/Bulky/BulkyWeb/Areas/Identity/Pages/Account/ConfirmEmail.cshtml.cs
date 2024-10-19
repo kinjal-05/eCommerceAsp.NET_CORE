cfa34c0747c336c21fe9671299e92b055c3b20f8
kinjal-05 <165459555+kinjal-05@users.noreply.github.com>
2024-10-20 02:27:57 +0530

    Update ConfirmEmail.cshtml.cs

--- ASP.NET-Core-MVC-NET-8-main/Bulky/BulkyWeb/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ ASP.NET-Core-MVC-NET-8-main/Bulky/BulkyWeb/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -1,6 +1,5 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
+.
+
 
 using System;
 using System.Linq;
@@ -23,10 +22,7 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
             _userManager = userManager;
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
+       
         [TempData]
         public string StatusMessage { get; set; }
         public async Task<IActionResult> OnGetAsync(string userId, string code)
