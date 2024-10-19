a8c49145913e6be5c96667646c2e2731d240bd88
kinjal-05 <165459555+kinjal-05@users.noreply.github.com>
2024-10-20 02:25:13 +0530

    Update RegisterConfirmation.cshtml.cs

--- ASP.NET-Core-MVC-NET-8-main/Bulky/BulkyWeb/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ ASP.NET-Core-MVC-NET-8-main/Bulky/BulkyWeb/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -1,6 +1,3 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
 
 using System;
 using System.Text;
@@ -26,22 +23,12 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
             _sender = sender;
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
+        
         public string Email { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
+       
         public bool DisplayConfirmAccountLink { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         public string EmailConfirmationUrl { get; set; }
 
         public async Task<IActionResult> OnGetAsync(string email, string returnUrl = null)
